dad8288d7ca932b1d36e449e4efe4fa5b352ea3a
Jason Smith <devjason@gmail.com>
2024-03-05 00:21:58 -0600

    TrapExtender to PositionVoltage for accuracy

--- src/main/java/frc/robot/subsystems/trap/TrapExtender.java
+++ src/main/java/frc/robot/subsystems/trap/TrapExtender.java
@@ -2,10 +2,12 @@ package frc.robot.subsystems.trap;
 
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -15,19 +17,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
 import frc.robot.subsystems.Lifecycle;
-import frc.robot.subsystems.util.MotionMagicConfig;
-import frc.robot.subsystems.util.OpenLoopSpeedsConfig;
-import frc.robot.subsystems.util.PIDHelper;
-import frc.robot.subsystems.util.SoftLimitValues;
+import frc.robot.subsystems.util.*;
 
 public class TrapExtender implements Lifecycle, Sendable {
-    public static final double CLOSED_LOOP_TIMEOUT = 1.0;
+    public static final double CLOSED_LOOP_TIMEOUT = 2.0;
     private final TalonFX motor;
     private final TalonFXConfiguration configuration;
     private final OpenLoopSpeedsConfig openLoopSpeeds = new OpenLoopSpeedsConfig(-0.3, 0.3);
-    private final MotionMagicConfig motionMagicConfig = new MotionMagicConfig();
     private final DutyCycleOut openLoopOut = new DutyCycleOut(0);
-    private final MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0);
+    private final PositionVoltage positionVoltage = new PositionVoltage(0);
     private final PIDHelper pidHelper = new PIDHelper(Trap.SUBSYSTEM_NAME + "/TrapExtender/PID");
     private boolean openLoop = true;
     private double openLoopSetpoint = 0;
@@ -37,7 +35,6 @@ public class TrapExtender implements Lifecycle, Sendable {
 
     private final SoftLimitValues softLimits = new SoftLimitValues(0.55, -24.25);
     private boolean softLimitsEnabled = true;
-
     private double inPositionThreshold = 0.5;
 
     private final Timer closedLoopSetTimer = new Timer();
@@ -50,14 +47,11 @@ public class TrapExtender implements Lifecycle, Sendable {
                         new SoftwareLimitSwitchConfigs()
                                 .withReverseSoftLimitThreshold(softLimits.reverseSoftLimitThreshold()).withReverseSoftLimitEnable(true)   // 0.25 = 1/8"
                                 .withForwardSoftLimitThreshold(softLimits.forwardSoftLimitThreshold()).withForwardSoftLimitEnable(true)
-                ).withHardwareLimitSwitch(new HardwareLimitSwitchConfigs().withForwardLimitEnable(false).withReverseLimitEnable(false));
-        pidHelper.initialize(2.6, 0.6, 0, 0, 0, 0);
-        motionMagicConfig.setJerk(2000);
-        motionMagicConfig.setVelocity(60);
-        motionMagicConfig.setAcceleration(300);
-        updatePIDFromDashboard();
-        pidHelper.updateConfiguration(configuration.Slot0);
-        motionMagicConfig.updateSlot0Config(configuration.Slot0);
+                ).withHardwareLimitSwitch(new HardwareLimitSwitchConfigs().withForwardLimitEnable(false).withReverseLimitEnable(false))
+                .withSlot0(new Slot0Configs().withKP(12.0));
+//        pidHelper.initialize(2.6, 0.6, 0, 0, 0, 0);
+//        pidHelper.initialize(12, 0, 0, 0, 0, 0);
+//        pidHelper.updateConfiguration(configuration.Slot0);
         motor.getConfigurator().apply(configuration);
     }
 
@@ -133,12 +127,14 @@ public class TrapExtender implements Lifecycle, Sendable {
     }
 
     public void setClosedLoopSetpoint(double closedLoopSetpoint) {
+        BSLogger.log("TrapExtender", "setClosedLoopSetpoint: " + closedLoopSetpoint);
         this.closedLoopSetTimer.restart();
         this.setOpenLoop(false);
         this.closedLoopSetpoint = closedLoopSetpoint;
     }
 
     public void setTrapPosition(TrapPosition trapPosition) {
+        BSLogger.log("TrapExtender", "setTrapPosition: " + trapPosition);
         this.trapPosition = trapPosition;
         this.setClosedLoopSetpoint(trapPosition.setpoint);
     }
@@ -149,8 +145,8 @@ public class TrapExtender implements Lifecycle, Sendable {
 
     public void updatePIDFromDashboard() {
         pidHelper.updateConfiguration(configuration.Slot0);
-        motionMagicConfig.updateSlot0Config(configuration.Slot0);
-        motionMagicConfig.updateMotionMagicConfig(configuration.MotionMagic);
+//        motionMagicConfig.updateSlot0Config(configuration.Slot0);
+//        motionMagicConfig.updateMotionMagicConfig(configuration.MotionMagic);
         StatusCode result = motor.getConfigurator().apply(configuration);
         DataLogManager.log("[TrapExtender] update PID info from dash, result: " + result);
     }
@@ -163,22 +159,24 @@ public class TrapExtender implements Lifecycle, Sendable {
     public void periodic() {
         // Check whether to expire closed loop control request
         if (closedLoopSetTimer.hasElapsed(CLOSED_LOOP_TIMEOUT) && !openLoop) {
+            BSLogger.log("TrapExtend", "Exiting closed loop");
             openLoop = true;
             closedLoopSetTimer.stop();
         }
 
         // Check whether to switch to open loop
-        if (!openLoop && isInPosition()) {
-            openLoop = true;
-            closedLoopSetTimer.stop();
-            openLoopSetpoint = 0.0;
-        }
+//        if (!openLoop && isInPosition()) {
+//            openLoop = true;
+//            closedLoopSetTimer.stop();
+//            openLoopSetpoint = 0.0;
+//        }
 
         if (openLoop) {
             motor.setControl(openLoopOut.withOutput(openLoopSetpoint));
         } else {
 //            updatePIDFromDashboard();
-            motor.setControl(motionMagicVoltage.withPosition(closedLoopSetpoint));
+//            motor.setControl(motionMagicVoltage.withPosition(closedLoopSetpoint));
+            motor.setControl(positionVoltage.withPosition(closedLoopSetpoint));
         }
     }
 
@@ -192,12 +190,6 @@ public class TrapExtender implements Lifecycle, Sendable {
         if (Constants.Dashboard.ConfigurationMode || Constants.Dashboard.TrapConfigMode) {
             builder.addDoubleProperty("TrapExtender/UpSpeed", openLoopSpeeds::getUpSpeed, openLoopSpeeds::setUpSpeed);
             builder.addDoubleProperty("TrapExtender/DownSpeed", openLoopSpeeds::getDownSpeed, openLoopSpeeds::setDownSpeed);
-
-            builder.addDoubleProperty("TrapExtender/MM/kS", motionMagicConfig::getkS, motionMagicConfig::setkS);
-            builder.addDoubleProperty("TrapExtender/MM/kG", motionMagicConfig::getkG, motionMagicConfig::setkG);
-            builder.addDoubleProperty("TrapExtender/MM/Acceleration", motionMagicConfig::getAcceleration, motionMagicConfig::setAcceleration);
-            builder.addDoubleProperty("TrapExtender/MM/Velocity", motionMagicConfig::getVelocity, motionMagicConfig::setVelocity);
-            builder.addDoubleProperty("TrapExtender/MM/Jerk", motionMagicConfig::getJerk, motionMagicConfig::setJerk);
 
             builder.addBooleanProperty("TrapExtender/FwdLimitHit", () -> this.motor.getFault_ForwardSoftLimit().getValue(), null);
             builder.addBooleanProperty("TrapExtender/RevLimitHit", () -> this.motor.getFault_ReverseSoftLimit().getValue(), null);
